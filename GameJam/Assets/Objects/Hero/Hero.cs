055e37f5118f01d588fcb63b99831200612c763b
OdgeM <168043240+OdgeM@users.noreply.github.com>
2026-01-30 19:26:33 +0000

    Modular Sprites

---
+++ GameJam/Assets/Objects/Hero/Hero.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    public string heroName;
+    public string realName;
+    public int age;
+    public string hometown;
+    public string history;
+}
